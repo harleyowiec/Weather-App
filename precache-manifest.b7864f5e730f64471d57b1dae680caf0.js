43ca5cc018309b45c5fae86cff11067fe3c67d61
Bartek Starzec <bartoszstarzec1@gmail.com>
2019-04-17 22:09:21 +0200

    Updates

---
+++ precache-manifest.b7864f5e730f64471d57b1dae680caf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "993ba4703525ad8b05dee1c0335447cd",
+    "url": "/Weather-App/static/media/day-and-night-1228029.993ba470.jpg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/Weather-App/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/Weather-App/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "f9c84d40020a21a1167a",
+    "url": "/Weather-App/static/js/runtime~main.f9c84d40.js"
+  },
+  {
+    "revision": "9b5277c551a9c4964e30",
+    "url": "/Weather-App/static/js/main.9b5277c5.chunk.js"
+  },
+  {
+    "revision": "a60652067b5355dbb5ba",
+    "url": "/Weather-App/static/js/2.a6065206.chunk.js"
+  },
+  {
+    "revision": "9b5277c551a9c4964e30",
+    "url": "/Weather-App/static/css/main.8ed7a591.chunk.css"
+  },
+  {
+    "revision": "b4ee8e56c101b0cf9c4b3ae21e84e9b4",
+    "url": "/Weather-App/index.html"
+  }
+];
